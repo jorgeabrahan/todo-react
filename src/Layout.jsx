6210f392ede3d8850fb908aed70cea6e2bc0a3d8
jorgeabrahan <jorge24abrahan@gmail.com>
2023-02-20 18:06:31 -0600

    Include outlet component to layout

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,8 +1,11 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { Outlet } from 'react-router-dom';
 import Navbar from './components/Navbar';
 
 const Layout = () => (
   <div className="wrapper">
     <Navbar />
+    <Outlet />
   </div>
 );
 export default Layout;
