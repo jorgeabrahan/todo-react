8292b70ca2267791d32279f6c73f8939bb529dad
jorgeabrahan <jorge24abrahan@gmail.com>
2023-02-20 21:38:08 -0600

    Add logout button functionality on navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
 import { NavLink } from 'react-router-dom';
-
+import { useAuthContext } from '../context/AuthContext';
 const links = [
   { path: '/', text: 'Home' },
   { path: 'about', text: 'About' },
@@ -8,22 +8,21 @@ const links = [
   { path: 'login', text: 'Login' },
 ];
 
-const Navbar = () => (
-  <nav className="navbar">
-    <ul>
-      {links.map((link) => (
-        <li key={link.text}>
-          <NavLink
-            to={link.path}
-            style={({ isActive }) => ({
-              color: isActive ? 'red' : undefined,
-            })}
-          >
-            {link.text}
-          </NavLink>
-        </li>
-      ))}
-    </ul>
-  </nav>
-);
+const Navbar = () => {
+  const { user, logout } = useAuthContext();
+  const handleLogout = () => {
+    logout();
+  };
+  return (
+    <>
+      <nav className="navbar">{/* ... */}</nav>
+      {user && (
+        <div className="logout">
+          <p>{user}</p>
+          <button type="button" onClick={handleLogout}>Logout</button>
+        </div>
+      )}
+    </>
+  );
+};
 export default Navbar;
