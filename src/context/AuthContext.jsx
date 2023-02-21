f9094e6ad8a479d86e74c0fcfd2cab25a9136676
jorgeabrahan <jorge24abrahan@gmail.com>
2023-02-20 23:44:31 -0600

    Persist user state in browser storage

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,10 +1,23 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
 import PropTypes from 'prop-types';
-import { useState, useContext, createContext } from 'react';
+import {
+  useState, useEffect, useContext, createContext,
+} from 'react';
 
 const AuthContext = createContext(null);
 export const AuthProvider = ({ children }) => {
-  const [user, setUser] = useState('');
+  function getUsername() {
+    // getting stored state
+    const temp = localStorage.getItem('username');
+    const savedUsername = JSON.parse(temp);
+    return savedUsername || '';
+  }
+  const [user, setUser] = useState(getUsername());
+  useEffect(() => {
+    // storing user state
+    const temp = JSON.stringify(user);
+    localStorage.setItem('username', temp);
+  }, [user]);
   const login = (user) => setUser(user);
   const logout = () => setUser(null);
   return (
