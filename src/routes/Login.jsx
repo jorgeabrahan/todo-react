434c4376e99df86ecb2ee02130be9749c55e8839
jorgeabrahan <jorge24abrahan@gmail.com>
2023-02-20 19:20:29 -0600

    Create login component and styles

--- src/routes/Login.jsx
+++ src/routes/Login.jsx
@@ -1,2 +1,28 @@
-const Login = () => <div>Login page</div>;
+import { useState } from 'react';
+import styles from '../styles/modules/Login.module.scss';
+
+const Login = () => {
+  const [username, setUsername] = useState('');
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!username) return;
+    console.log(username);
+  };
+  return (
+    <div>
+      <h1>Login</h1>
+      <div className={styles.formWrapper}>
+        <form className={styles.form} onSubmit={handleSubmit}>
+          <input
+            type="text"
+            placeholder="username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+          <button type="button">Login</button>
+        </form>
+      </div>
+    </div>
+  );
+};
 export default Login;
