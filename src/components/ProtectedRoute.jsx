11914951bcebc57fa047af41db7cd449e6b858bb
jorgeabrahan <jorge24abrahan@gmail.com>
2023-02-20 22:55:59 -0600

    Redirect the user to the intendend page to visit after logging in

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -1,12 +1,19 @@
 /* eslint-disable import/no-extraneous-dependencies */
-import { Navigate } from 'react-router-dom';
+import { Navigate, useLocation } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { useAuthContext } from '../context/AuthContext';
 
 const ProtectedRoute = ({ children }) => {
   const { user } = useAuthContext();
+  const location = useLocation();
   if (!user) {
-    return <Navigate to="/login" />;
+    return (
+      <Navigate
+        to="/login"
+        state={{ pathname: location.pathname }}
+        replace
+      />
+    );
   }
   return children;
 };
--- src/routes/Login.jsx
+++ src/routes/Login.jsx
@@ -1,19 +1,21 @@
 import { useState } from 'react';
 // eslint-disable-next-line import/no-extraneous-dependencies
-import { useNavigate } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 import { useAuthContext } from '../context/AuthContext';
 import styles from '../styles/modules/Login.module.scss';
 
 const Login = () => {
   const [username, setUsername] = useState('');
   const { login } = useAuthContext();
+  const location = useLocation();
+  const from = location.state?.pathname || '/';
   const navigate = useNavigate();
   const handleSubmit = (e) => {
     e.preventDefault();
     if (!username) return;
     login(username);
     setUsername('');
-    navigate('/', { replace: true });
+    navigate(from, { replace: true });
   };
   return (
     <div>
