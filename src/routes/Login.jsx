ad9fe04a86a1f73cc50ccfff6e7f323775c25873
jorgeabrahan <jorge24abrahan@gmail.com>
2023-02-20 22:00:08 -0600

    Navigate away from the login screen after the user logs in

--- src/routes/Login.jsx
+++ src/routes/Login.jsx
@@ -1,15 +1,19 @@
 import { useState } from 'react';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { useNavigate } from 'react-router-dom';
 import { useAuthContext } from '../context/AuthContext';
 import styles from '../styles/modules/Login.module.scss';
 
 const Login = () => {
   const [username, setUsername] = useState('');
   const { login } = useAuthContext();
+  const navigate = useNavigate();
   const handleSubmit = (e) => {
     e.preventDefault();
     if (!username) return;
     login(username);
     setUsername('');
+    navigate('/', { replace: true });
   };
   return (
     <div>
