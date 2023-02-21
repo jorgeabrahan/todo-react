0da552c466a8b9e88e9ee3139a691305d811e748
jorgeabrahan <jorge24abrahan@gmail.com>
2023-02-20 21:23:58 -0600

    Wrap layout content in AuthProvider component

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,11 +1,14 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
 import { Outlet } from 'react-router-dom';
 import Navbar from './components/Navbar';
+import { AuthProvider } from './context/AuthContext';
 
 const Layout = () => (
   <div className="wrapper">
-    <Navbar />
-    <Outlet />
+    <AuthProvider>
+      <Navbar />
+      <Outlet />
+    </AuthProvider>
   </div>
 );
 export default Layout;
