1a5e6d158d63e53897c329c8b937fafe8839d35d
jorgeabrahan <jorge24abrahan@gmail.com>
2023-02-20 18:15:17 -0600

    Add links to navbar and create NavLinks

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,2 +1,26 @@
-const Navbar = () => <div>Navigation items here</div>;
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { NavLink } from 'react-router-dom';
+
+const links = [
+  { path: '/', text: 'Home' },
+  { path: 'about', text: 'About' },
+  { path: 'profile', text: 'Profile' },
+  { path: 'login', text: 'Login' },
+];
+
+const Navbar = () => (
+  <nav className="navbar">
+    <ul>
+      {links.map((link) => (
+        <li key={link.text}>
+          <NavLink
+            to={link.path}
+          >
+            {link.text}
+          </NavLink>
+        </li>
+      ))}
+    </ul>
+  </nav>
+);
 export default Navbar;
